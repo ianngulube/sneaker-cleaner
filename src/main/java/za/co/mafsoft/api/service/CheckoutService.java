e3edf06d9f86b36097a5291f49f8cbd0e7abefe5
Ian Ngulube <ngulubeian94@gmail.com>
2025-03-25 21:46:17 +0200

    refactor:service:add cart validation

--- src/main/java/za/co/mafsoft/api/service/CheckoutService.java
+++ src/main/java/za/co/mafsoft/api/service/CheckoutService.java
@@ -7,12 +7,17 @@ import za.co.mafsoft.api.model.Catalog;
 
 import java.math.BigDecimal;
 import java.util.Map;
+import java.util.Objects;
+import java.util.zip.DataFormatException;
 
 @Slf4j
 @ApplicationScoped
 public class CheckoutService {
 
-    public void checkout(final Cart cart) {
+    public void checkout(final Cart cart) throws DataFormatException {
+        if (!validateCart(cart)) {
+            throw new DataFormatException("Cart is missing important data");
+        }
         BigDecimal totalPrice = BigDecimal.valueOf(0);
         for (Map.Entry<Catalog, Integer> entry : cart.getCatalogCount().entrySet()) {
             Catalog catalog = entry.getKey();
@@ -20,5 +25,14 @@ public class CheckoutService {
             totalPrice = totalPrice.add(catalog.getPrice().multiply(BigDecimal.valueOf(integer)));
         }
         cart.setCalculatedTotalPrice(totalPrice);
+    }
+
+    private boolean validateCart(final Cart cart) {
+        if (Objects.isNull(cart)) {
+            return false;
+        }
+        return !Objects.isNull(cart.getCatalogCount()) &&
+                !Objects.isNull(cart.getUser()) &&
+                !Objects.isNull(cart.getItemTransport());
     }
 }
