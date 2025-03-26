8826946e6406a7b15fef7ba7c5708d05a6dc7193
Ian Ngulube <ngulubeian94@gmail.com>
2025-03-26 14:15:58 +0200

    refactor:entity:add jsonb address field

--- src/main/java/za/co/mafsoft/api/entity/ItemEntity.java
+++ src/main/java/za/co/mafsoft/api/entity/ItemEntity.java
@@ -1,10 +1,14 @@
 package za.co.mafsoft.api.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import za.co.mafsoft.api.model.Address;
 import za.co.mafsoft.api.model.enums.ItemTransport;
 import za.co.mafsoft.api.model.enums.ItemType;
 
@@ -26,4 +30,7 @@ public class ItemEntity extends PanacheEntity {
     private LocalDateTime dateDroppedOff;
     private LocalDateTime dateDelivered;
     private ItemTransport itemTransport;
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private Address address;
 }
