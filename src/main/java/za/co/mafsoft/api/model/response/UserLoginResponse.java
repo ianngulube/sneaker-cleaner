e5984f4fa4adc5951c9c8e7ab7484e57a92d646f
Ian Ngulube <ngulubeian94@gmail.com>
2025-03-27 23:36:22 +0200

    refactor:model:add user field

--- src/main/java/za/co/mafsoft/api/model/response/UserLoginResponse.java
+++ src/main/java/za/co/mafsoft/api/model/response/UserLoginResponse.java
@@ -2,6 +2,7 @@ package za.co.mafsoft.api.model.response;
 
 import lombok.Builder;
 import lombok.Data;
+import za.co.mafsoft.api.model.User;
 
 @Data
 @Builder
@@ -9,4 +10,5 @@ public class UserLoginResponse {
     public Integer responseCode;
     private String responseDescription;
     private String authToken;
+    private User user;
 }
