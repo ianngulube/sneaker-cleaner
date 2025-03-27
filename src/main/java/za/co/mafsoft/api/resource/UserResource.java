b48ff050084d444f1c90f9e8eab535ab201d7c85
Ian Ngulube <ngulubeian94@gmail.com>
2025-03-27 18:51:13 +0200

    refactor:resource:cleanup

--- src/main/java/za/co/mafsoft/api/resource/UserResource.java
+++ src/main/java/za/co/mafsoft/api/resource/UserResource.java
@@ -1,6 +1,7 @@
 package za.co.mafsoft.api.resource;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -9,24 +10,25 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import za.co.mafsoft.api.model.User;
 import za.co.mafsoft.api.model.UserLogin;
+import za.co.mafsoft.api.model.response.UserCreateResponse;
 import za.co.mafsoft.api.service.interfaces.IUserService;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
     @Inject
     IUserService userService;
 
     @Path("/create")
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
     public Response createUser(final User user) {
-        userService.createUser(user);
-        return Response.ok().build();
+        UserCreateResponse createResponse = userService.createUser(user);
+        return Response.ok(createResponse).build();
     }
 
     @Path("/verify-user/{email-or-msisdn}/{verification-code}")
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
     public Response verifyUser(@PathParam("email-or-msisdn") final String emailOrMsisdn,
                                @PathParam("verification-code") final String verificationCode) {
         userService.verifyUser(emailOrMsisdn, verificationCode);
@@ -35,7 +37,6 @@ public class UserResource {
 
     @Path("/login")
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
     public Response login(final UserLogin userLogin) {
         Boolean result = userService.login(userLogin);
         return Response.status(Response.Status.OK).entity(result).build();
