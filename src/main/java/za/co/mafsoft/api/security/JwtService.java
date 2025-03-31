24e9c9df861ec58f6d29949379b9b4a62e1f39de
Ian Ngulube <ngulubeian94@gmail.com>
2025-03-31 21:23:40 +0200

    refactor:service:cleanup

--- src/main/java/za/co/mafsoft/api/security/JwtService.java
+++ src/main/java/za/co/mafsoft/api/security/JwtService.java
@@ -2,7 +2,6 @@ package za.co.mafsoft.api.security;
 
 import io.smallrye.jwt.build.Jwt;
 import jakarta.enterprise.context.ApplicationScoped;
-import org.eclipse.microprofile.jwt.Claims;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -14,7 +13,6 @@ public class JwtService {
         return Jwt.issuer("Sneaker Cleaner API Authenticator")
                 .upn(userName)
                 .groups(new HashSet<>(Arrays.asList("User", "Admin")))
-                .claim(Claims.exp.name(), System.currentTimeMillis() + 1000 * 60 * 5)
                 .sign();
     }
 
